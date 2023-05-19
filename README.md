1a6e4292c8f5490667616aa69e00ad5d64c63a59
mikolajszydlo <71712385+mikolajszydlo@users.noreply.github.com>
2023-05-19 16:10:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# firebase-web-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gwv1up)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import firebase from './FirebaseConfig';
+
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase recipies</h1>
+      </div>
+    </div>
+  );
+}
